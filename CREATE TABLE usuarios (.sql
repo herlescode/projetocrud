dcd82a2f40e4347ffa9f65bb7c6533c1ed136a5e
Herles Pontual <herles.pontual@presidencia.gov.br>
2026-03-17 17:01:46 -0300

    Meu primeiro commit

---
+++ CREATE TABLE usuarios (.sql
@@ -0,0 +1,15 @@
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuarios (nome, senha) VALUES ('camila', '123456');
+
+SELECT * FROM usuarios;
+
+ALTER TABLE usuarios ADD COLUMN adm BOOLEAN DEFAULT FALSE;
+
+UPDATE usuarios
+SET adm = TRUE
+WHERE id = 1;
